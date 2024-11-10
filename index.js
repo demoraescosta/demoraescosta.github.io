b5b9a8d3b6b4ace6d79673faeb02cee28215b227
André de Moraes Costa <an.moraescosta@gmail.com>
2024-11-10 15:37:42 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,208 @@
+let modo_noturno = false;
+
+function ativarModoNoturno()
+{
+    document.documentElement.style.setProperty('--texto', 'var(--texto-noturno)');
+    document.documentElement.style.setProperty('--fundo', 'var(--fundo-noturno)');
+    document.documentElement.style.setProperty('--border', 'var(--border-noturno)');
+    document.documentElement.style.setProperty('--link', 'var(--link-noturno)');
+
+}
+function desativarModoNoturno()
+{
+    document.documentElement.style.setProperty('--texto', '#0c0c0c');
+    document.documentElement.style.setProperty('--fundo', '#f8f8f8');
+    document.documentElement.style.setProperty('--border', '#cccccc');
+    document.documentElement.style.setProperty('--link', '#5c5c5c');
+
+}
+
+function toggleModoNoturno()
+{
+    (modo_noturno) ? ativarModoNoturno() : desativarModoNoturno();
+}
+
+let btnModoNoturno = document.getElementById('btnModoNoturno');
+    
+btnModoNoturno.addEventListener('click', () => 
+{
+    modo_noturno = modo_noturno ? false : true;
+    toggleModoNoturno();
+});
+
+// categoria selecionada
+const categorias =
+{
+    Esportes: 0,
+    Politica: 1,
+    Cultura:  2
+};
+
+const manchetes = 
+{
+    Esportes: 
+    [ 
+        [
+            "West Ham sinaliza com oferta de R$ 225 milhões pelo atacante Igor Jesus, do Botafogo",
+            "Jogador é procurado por time da Premier League"
+        ],
+
+        [
+            "Bola de Ouro 2024: Rodri é eleito o melhor jogador do mundo, e Vini Jr. é segundo",
+            "Meio-campista do Manchester City quebra jejum de 64 anos da Espanha e desbanca atacante brasileiro do Real Madrid, apontado como favorito e ausente na cerimônia"
+        ],
+
+        [
+            "Atleta Bruno Henrique é alvo de operação policial após indicios de manipulação de resultados (Xiiiii)",
+            "KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK"
+        ],
+
+        [
+            "Raphinha do Barcelona se destaca em gols/assistências na Champions League",
+            "Brasileiro é peça chave para o time de Hansi Flick",
+            "imagens/raphinha.webp"
+        ]
+    ],
+    Politica: 
+    [ 
+
+        [
+            "Bola de Ouro 2024: Rodri é eleito o melhor jogador do mundo, e Vini Jr. é segundo",
+            "Meio-campista do Manchester City quebra jejum de 64 anos da Espanha e desbanca atacante brasileiro do Real Madrid, apontado como favorito e ausente na cerimônia"
+        ],
+
+        [
+            "Atleta Bruno Henrique é alvo de operação policial após indicios de manipulação de resultados (Xiiiii)",
+            "KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK"
+        ],
+
+        [
+            "Atleta Bruno Henrique é alvo de operação policial após indicios de manipulação de resultados (Xiiiii)",
+            "KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK"
+        ],
+
+        [
+            "EUA terminam contagem de delegados: Trump 312 X 226 Kamala",
+            "Republicano venceu em todos os 7 Estados-pêndulo, inclusive no Arizona, o último a ter resultados divulgados na noite de sábado (9.nov.2024)",
+            "imagens/trump.webp"
+        ]
+    ],
+    Cultura: 
+    [ 
+        [
+            "West Ham sinaliza com oferta de R$ 225 milhões pelo atacante Igor Jesus, do Botafogo",
+            "Jogador é procurado por time da Premier League"
+        ],
+
+        [
+            "Bola de Ouro 2024: Rodri é eleito o melhor jogador do mundo, e Vini Jr. é segundo",
+            "Meio-campista do Manchester City quebra jejum de 64 anos da Espanha e desbanca atacante brasileiro do Real Madrid, apontado como favorito e ausente na cerimônia"
+        ],
+
+        [
+            "Bola de Ouro 2024: Rodri é eleito o melhor jogador do mundo, e Vini Jr. é segundo",
+            "Meio-campista do Manchester City quebra jejum de 64 anos da Espanha e desbanca atacante brasileiro do Real Madrid, apontado como favorito e ausente na cerimônia"
+        ],
+
+        [
+            "Atleta Bruno Henrique é alvo de operação policial após indicios de manipulação de resultados (Xiiiii)",
+            "KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK"
+        ]
+    ],
+}
+
+// alterar machetes dependendo da categoria
+
+let titulo1 = document.getElementById("titulo1")
+let titulo2 = document.getElementById("titulo2")
+let titulo3 = document.getElementById("titulo3")
+
+let titulo4 = document.getElementById("titulo4");
+
+let subtitulo1 = document.getElementById("subtitulo1")
+let subtitulo2 = document.getElementById("subtitulo2")
+let subtitulo3 = document.getElementById("subtitulo3")
+
+let subtitulo4 = document.getElementById("subtitulo4");
+
+let cat_atual = categorias.Esportes;
+
+function atualizarManchetes()
+{
+    switch (cat_atual)
+    {
+        case categorias.Esportes:
+            titulo1.innerText = manchetes.Esportes[0][0];
+            titulo2.innerText = manchetes.Esportes[1][0];
+            titulo3.innerText = manchetes.Esportes[2][0];
+
+            titulo4.innerText = manchetes.Esportes[3][0];
+
+            subtitulo1.innerText = manchetes.Esportes[0][1];
+            subtitulo2.innerText = manchetes.Esportes[1][1];
+            subtitulo3.innerText = manchetes.Esportes[2][1];
+
+            subtitulo4.innerText = manchetes.Esportes[3][1];
+            break;
+
+        case categorias.Politica:
+            titulo1.innerText = manchetes.Politica[0][0];
+            titulo2.innerText = manchetes.Politica[1][0];
+            titulo3.innerText = manchetes.Politica[2][0];
+
+            titulo4.innerText = manchetes.Politica[3][0];
+
+            subtitulo1.innerText = manchetes.Politica[0][1];
+            subtitulo2.innerText = manchetes.Politica[1][1];
+            subtitulo3.innerText = manchetes.Politica[2][1];
+            
+            subtitulo4.innerText = manchetes.Politica[3][1];
+
+            document.getElementById("imagem-destaque").src = manchetes.Politica[3][2];
+            break;
+
+        case categorias.Cultura:
+            titulo1.innerText = manchetes.Cultura[0][0];
+            titulo2.innerText = manchetes.Cultura[1][0];
+            titulo3.innerText = manchetes.Cultura[2][0];
+
+            titulo4.innerText = manchetes.Cultura[3][0];
+
+            subtitulo1.innerText = manchetes.Cultura[0][1];
+            subtitulo2.innerText = manchetes.Cultura[1][1];
+            subtitulo3.innerText = manchetes.Cultura[2][1];
+            
+            subtitulo4.innerText = manchetes.Cultura[3][1];
+
+            titulo4.src = String(manchetes.Cultura[3][2]);
+            break;
+        default:
+            break;
+    }
+}
+
+atualizarManchetes();
+
+let btnEsportes = document.getElementById('btnEsportes');
+
+btnEsportes.addEventListener('click', () => 
+{
+    cat_atual = categorias.Esportes;
+    atualizarManchetes();
+});
+
+let btnPolítica = document.getElementById('btnPolítica');
+
+btnPolítica.addEventListener('click', () => 
+{
+    cat_atual = categorias.Politica;
+    atualizarManchetes();
+});
+
+let btnCultura = document.getElementById('btnCultura');
+
+btnCultura.addEventListener('click', () => 
+{
+    cat_atual = categorias.Cultura;
+    atualizarManchetes();
+});
